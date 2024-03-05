c03ffaa6f22b6fc419c18e41d99c4e8c49d28069
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2024-03-05 23:10:01 +0530

    fixed date & time issue in ClassDetails section

--- src/components/ClassDetails/ClassDetails.tsx
+++ src/components/ClassDetails/ClassDetails.tsx
@@ -25,10 +25,11 @@ const days = [
 const ClassDetails = () => {
   const { selectedSection, isRoutineAvailable, isRoutineLoading, isRoutineError, routine } = useContext(RoutineContext);
   const classDays = useRef<string[]>([]);
+  const startingClass = useRef<IRoutine[]>([]);
 
   const date = new Date();
   const currentDay = days[date.getDay()];
-  const currentTime = `${date.getHours()}:${date.getMinutes()}`;
+  const currentTime = date.toLocaleTimeString();
 
   if (isRoutineLoading) return (
     <section className='class-details-container'>
@@ -52,29 +53,36 @@ const ClassDetails = () => {
     <section className='class-details-container'>
       {
         routine.map((result: IRoutine) => {
-          if (!classDays.current.includes(result.day_name)) {
+          if (!classDays.current.includes(result.day_name) && result.subject_name !== '---') {
             classDays.current.push(result.day_name);
+            startingClass.current.push(result);
           }
           const nextClass = routine[routine.indexOf(result) + 1];
-          if (result.day_name === currentDay && result.start_time <= currentTime && result.end_time >= currentTime) {
+          if (result.day_name == currentDay && ((new Date('2024-01-01 ' + result.start_time)).getTime()) <= ((new Date('2024-01-01 ' + currentTime)).getTime()) && ((new Date('2024-01-01 ' + result.end_time)).getTime()) >= ((new Date('2024-01-01 ' + currentTime)).getTime())) {
             return (
               <>
-                <div>
-                  <h4>Current Class</h4>
-                  <p>{result.subject_name}</p>
-                  <p>By {result.teacher_name}</p>
-                  <p>At {result.room_number}</p>
-                  <p>From {result.start_time}</p>
-                  <p>To {result.end_time}</p>
-                </div>
-                <div>
-                  <h4>Next Class</h4>
-                  <p>{nextClass.subject_name}</p>
-                  <p>By {nextClass.teacher_name}</p>
-                  <p>At {nextClass.room_number}</p>
-                  <p>From {nextClass.start_time}</p>
-                  <p>To {nextClass.end_time}</p>
-                </div>
+                {
+                  (result.subject_name !== '---') &&
+                  <div key={result.subject_name}>
+                    <h4>Current Class</h4>
+                    <p>{result.subject_name}</p>
+                    <p>By {result.teacher_name}</p>
+                    <p>At {result.room_number}</p>
+                    <p>From {result.start_time.substring(0, 5)}</p>
+                    <p>To {result.end_time.substring(0, 5)}</p>
+                  </div>
+                }
+                {
+                  (result.day_name === nextClass.day_name && nextClass.subject_name !== '---') &&
+                  <div key={result.teacher_name}>
+                    <h4>Next Class</h4>
+                    <p>{nextClass.subject_name}</p>
+                    <p>By {nextClass.teacher_name}</p>
+                    <p>At {nextClass.room_number}</p>
+                    <p>From {nextClass.start_time.substring(0, 5)}</p>
+                    <p>To {nextClass.end_time.substring(0, 5)}</p>
+                  </div>
+                }
               </>
             )
           }
@@ -82,12 +90,48 @@ const ClassDetails = () => {
       }
       {
         days.map((day) => {
-          if (!classDays.current.includes(day) && day === currentDay)
+          if (!classDays.current.includes(day) && day === currentDay) {
             return (
               <div key={day}>
                 <h4>No Classes Today</h4>
               </div>
             )
+          }
+        })
+      }
+      {
+        days.map((day) => {
+          if (day === currentDay && (new Date('2024-01-01 8:00:00')).getTime() >= (new Date('2024-01-01 ' + currentTime)).getTime()) {
+            return startingClass.current.map((result) => {
+              if (result.day_name == currentDay)
+                return (
+                  <div key={result.room_number}>
+                    <h4>Upcoming Class</h4>
+                    <p>{result.subject_name}</p>
+                    <p>By {result.teacher_name}</p>
+                    <p>At {result.room_number}</p>
+                    <p>From {result.start_time}</p>
+                    <p>To {result.end_time}</p>
+                  </div>
+                )
+            })
+          }
+          if (day === currentDay && (new Date('2024-01-01 16:40:00')).getTime() <= (new Date('2024-01-01 ' + currentTime)).getTime()) {
+            return startingClass.current.map((result) => {
+              if (result.day_name == days[days.indexOf(currentDay) + 1])
+                return (
+                  <div key={result.room_number}>
+                    <h4>Upcoming Class</h4>
+                    <p>{result.subject_name}</p>
+                    <p>By {result.teacher_name}</p>
+                    <p>At {result.room_number}</p>
+                    <p>From {result.start_time}</p>
+                    <p>To {result.end_time}</p>
+                    <p>On {result.day_name}</p>
+                  </div>
+                )
+            })
+          }
         })
       }
     </section>
